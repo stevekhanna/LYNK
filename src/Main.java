ad8de3ecde0ffe9cdaf57b842798f4566d0f7cd6
Steve Khanna <stvkhanna@gmail.com>
2019-02-16 16:18:09 -0700

    sqlServer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class main {
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = null;
+		conn = DriverManager.getConnection("jdbc:mysql://localhost/test","root","password");
+		System.out.println("Database is connected");
+		conn.close();
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	
+	
 }
