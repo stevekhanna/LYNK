8ee16d5a206e1a3f6a3b8cebf124a1aacf3a833f
issackj1 <issack.john1@ucalgary.ca>
2019-02-17 12:01:27 -0700

    mine

--- TheCompillers/settings.py
+++ TheCompillers/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'compillerwebsite.apps.CompillerwebsiteConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'parentHome'
+
+LOGIN_URL = 'login'
 
 STATICFILES_DIRS = [
     "../TheCompillers/static",
