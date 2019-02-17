753366317502252f816d1cc4241deb8ebabca666
issackj1 <issack.john1@ucalgary.ca>
2019-02-17 02:13:48 -0700

    Functionality restored

--- compillerwebsite/urls.py
+++ compillerwebsite/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='index'),
+    path('parentAttendance/', views.attendance, name='attendance'),
+    path('parentFeedback/', views.feedback, name='feedback'),
+    path('parentGrades/', views.grades, name='grades'),
+    path('p/', views.announcements, name='announcements'),
+    path('p/', views.agenda, name='agenda'),
 ]
--- compillerwebsite/views.py
+++ compillerwebsite/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, '../templates/index.html')
+
+def attendance(request):
+
+    return render(request, '../templates/parentAttendance.html')
+
+def feedback(request):
+
+    return render(request, '../templates/parentFeedback.html')
+
+def grades(request):
+
+    return render(request, '../templates/parentGrades.html')
+
+def announcements(request):
+
+    return render(request, '../templates/parentAnnouncements.html')
+
+def agenda(request):
+
+    return render(request, '../templates/parentAgenda.html')
